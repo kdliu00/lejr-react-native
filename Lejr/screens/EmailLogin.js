2fe40de16cc88a63badd98255c2ef7eff4955209
Kevin Liu <kdliu00@gmail.com>
2020-06-28 14:45:48 -0700

    Email login and validation done

--- Lejr/screens/EmailLogin.js
+++ Lejr/screens/EmailLogin.js
@@ -11,22 +11,12 @@ import auth from '@react-native-firebase/auth';
 import {Alert} from 'react-native';
 import * as yup from 'yup';
 
-var isSubmitting = false;
-
-var fieldErrorMessages = {
-  email: '',
-  password: '',
-  confirmPassword: '',
-};
-
 export default function EmailLogin({route, navigation}) {
-  console.log('Arrived at EmailLogin!');
-
   const {showConfirm} = route.params;
 
-  const [email, onEditEmail] = React.useState();
-  const [password, onEditPassword] = React.useState();
-  const [confirmPassword, onEditConfirmPassword] = React.useState();
+  const [email, setEmail] = React.useState();
+  const [password, setPassword] = React.useState();
+  const [confirmPassword, setConfirmPassword] = React.useState();
 
   const validationSchema = yup.object().shape({
     email: yup
@@ -48,14 +38,30 @@ export default function EmailLogin({route, navigation}) {
           .required();
       }
     }),
-    confirmPassword: yup
-      .string()
-      .required()
-      .label('Confirm password')
-      .test('passwords-match', 'Passwords must match', function(value) {
-        return this.parent.password === value;
-      }),
+    confirmPassword: yup.lazy(() => {
+      if (showConfirm) {
+        return yup
+          .string()
+          .required()
+          .label('Confirm password')
+          .test('passwords-match', 'Passwords must match', function(value) {
+            return password === value;
+          });
+      } else {
+        return yup.string();
+      }
+    }),
   });
+
+  const [emailError, setEmailError] = React.useState();
+  const [passwordError, setPasswordError] = React.useState();
+  const [confirmPasswordError, setConfirmPasswordError] = React.useState();
+
+  const [isSubmitting, setIsSubmitting] = React.useState();
+
+  const emailRef = React.createRef();
+  const passwordRef = React.createRef();
+  const confirmPasswordRef = React.createRef();
 
   return (
     <View style={styles.container}>
@@ -75,13 +81,29 @@ export default function EmailLogin({route, navigation}) {
               title="Sign Up"
               onPress={() => {
                 console.log('Signing up with email!');
-                isSubmitting = true;
-                validatedSignUp(
-                  validationSchema,
-                  email,
-                  password,
-                  confirmPassword,
-                );
+                validationSchema
+                  .validate({
+                    email: email,
+                    password: password,
+                    confirmPassword: confirmPassword,
+                  })
+                  .catch(error =>
+                    onValidationError(
+                      error.message,
+                      validationSchema,
+                      email,
+                      setEmailError,
+                      password,
+                      setPasswordError,
+                      confirmPassword,
+                      setConfirmPasswordError,
+                    ),
+                  )
+                  .then(valid => {
+                    if (valid) {
+                      signUp(email, password, setIsSubmitting);
+                    }
+                  });
               }}
             />
           </View>
@@ -91,8 +113,28 @@ export default function EmailLogin({route, navigation}) {
               title="Sign In"
               onPress={() => {
                 console.log('Signing in with email!');
-                isSubmitting = true;
-                validatedSignIn(validationSchema, email, password);
+                validationSchema
+                  .validate({
+                    email: email,
+                    password: password,
+                  })
+                  .catch(error =>
+                    onValidationError(
+                      error.message,
+                      validationSchema,
+                      email,
+                      setEmailError,
+                      password,
+                      setPasswordError,
+                      confirmPassword,
+                      setConfirmPasswordError,
+                    ),
+                  )
+                  .then(valid => {
+                    if (valid) {
+                      signIn(email, password, setIsSubmitting);
+                    }
+                  });
               }}
             />
           </View>
@@ -100,46 +142,50 @@ export default function EmailLogin({route, navigation}) {
       </View>
       <View style={styles.loginFields}>
         <InputField
-          fieldKey="email"
+          fieldError={emailError}
+          isSubmitting={isSubmitting}
+          refToPass={emailRef}
           placeholder="username@email.com"
           onChangeText={text => {
-            onEditEmail(text);
-            validationSchema
-              .validateAt('email', {email: email})
-              .catch(error => {
-                fieldErrorMessages.email = error.message;
-              });
+            setEmail(text);
+            validateEmail(validationSchema, text, setEmailError);
+          }}
+          onSubmitEditing={() => {
+            passwordRef.current.focus();
           }}
           value={email}
           autoFocus
         />
         <InputField
-          fieldKey="password"
+          fieldError={passwordError}
+          isSubmitting={isSubmitting}
+          refToPass={passwordRef}
           placeholder="password"
           onChangeText={text => {
-            onEditPassword(text);
-            validationSchema
-              .validateAt('password', {password: password})
-              .catch(error => {
-                fieldErrorMessages.password = error.message;
-              });
+            setPassword(text);
+            validatePassword(validationSchema, text, setConfirmPasswordError);
+          }}
+          onSubmitEditing={() => {
+            if (showConfirm) {
+              confirmPasswordRef.current.focus();
+            }
           }}
           value={password}
           secureTextEntry
         />
         {showConfirm && (
           <InputField
-            fieldKey="confirmPassword"
+            fieldError={confirmPasswordError}
+            isSubmitting={isSubmitting}
+            refToPass={confirmPasswordRef}
             placeholder="confirm password"
             onChangeText={text => {
-              onEditConfirmPassword(text);
-              validationSchema
-                .validateAt('confirmPassword', {
-                  confirmPassword: confirmPassword,
-                })
-                .catch(error => {
-                  fieldErrorMessages.confirmPassword = error.message;
-                });
+              setConfirmPassword(text);
+              validateConfirmPassword(
+                validationSchema,
+                text,
+                setConfirmPasswordError,
+              );
             }}
             value={confirmPassword}
             secureTextEntry
@@ -150,43 +196,12 @@ export default function EmailLogin({route, navigation}) {
   );
 }
 
-function validatedSignUp(validationSchema, email, password, confirmPassword) {
-  validationSchema
-    .validate({
-      email: email,
-      password: password,
-      confirmPassword: confirmPassword,
-    })
-    .then(valid => {
-      if (valid) {
-        signUp(email, password);
-      } else {
-        isSubmitting = false;
-      }
-    });
-}
-
-function validatedSignIn(validationSchema, email, password) {
-  validationSchema
-    .validate({
-      email: email,
-      password: password,
-      confirmPassword: password,
-    })
-    .then(valid => {
-      if (valid) {
-        signIn(email, password);
-      } else {
-        isSubmitting = false;
-      }
-    });
-}
-
-function signUp(email, password) {
+function signUp(email, password, setIsSubmitting) {
+  setIsSubmitting(true);
   auth()
     .createUserWithEmailAndPassword(email, password)
     .catch(error => {
-      onEmailLoginError(error);
+      onEmailLoginError(error, setIsSubmitting);
     })
     .then(
       () => {
@@ -194,15 +209,17 @@ function signUp(email, password) {
       },
       () => {
         console.warn('Sign up with email failed!');
+        setIsSubmitting(false);
       },
     );
 }
 
-function signIn(email, password) {
+function signIn(email, password, setIsSubmitting) {
+  setIsSubmitting(true);
   auth()
     .signInWithEmailAndPassword(email, password)
     .catch(error => {
-      onEmailLoginError(error);
+      onEmailLoginError(error, setIsSubmitting);
     })
     .then(
       () => {
@@ -210,11 +227,12 @@ function signIn(email, password) {
       },
       () => {
         console.warn('Sign in with email failed!');
+        setIsSubmitting(false);
       },
     );
 }
 
-function onEmailLoginError(error) {
+function onEmailLoginError(error, setIsSubmitting) {
   var errorCode = error.userInfo.code;
   var message = error.userInfo.message;
   var alertTitle = '<INSERT ALERT TITLE>';
@@ -235,25 +253,90 @@ function onEmailLoginError(error) {
   console.warn(errorCode);
   Alert.alert(alertTitle, message);
 
-  isSubmitting = false;
+  setIsSubmitting(false);
 }
 
-const InputFieldWrapper = ({fieldKey, children}) => (
+function onValidationError(
+  message,
+  validationSchema,
+  email,
+  setEmailError,
+  password,
+  setPasswordError,
+  confirmPassword,
+  setConfirmPasswordError,
+) {
+  console.warn(message);
+  validateEmail(validationSchema, email, setEmailError);
+  validatePassword(validationSchema, password, setPasswordError);
+  validateConfirmPassword(
+    validationSchema,
+    confirmPassword,
+    setConfirmPasswordError,
+  );
+}
+
+function validateEmail(validationSchema, text, setEmailError) {
+  validationSchema
+    .validateAt('email', {email: text})
+    .catch(error => {
+      setEmailError(error.message);
+    })
+    .then(valid => {
+      if (valid) {
+        setEmailError('');
+      }
+    });
+}
+
+function validatePassword(validationSchema, text, setPasswordError) {
+  validationSchema
+    .validateAt('password', {password: text})
+    .catch(error => {
+      setPasswordError(error.message);
+    })
+    .then(valid => {
+      if (valid) {
+        setPasswordError('');
+      }
+    });
+}
+
+function validateConfirmPassword(
+  validationSchema,
+  text,
+  setConfirmPasswordError,
+) {
+  validationSchema
+    .validateAt('confirmPassword', {confirmPassword: text})
+    .catch(error => {
+      setConfirmPasswordError(error.message);
+    })
+    .then(valid => {
+      if (valid) {
+        setConfirmPasswordError('');
+      }
+    });
+}
+
+const InputFieldWrapper = ({fieldError, children}) => (
   <View style={styles.textInputWrapper}>
-    <Text style={styles.errorText}>{fieldErrorMessages[fieldKey]}</Text>
+    <Text style={styles.errorText}>{fieldError}</Text>
     {children}
   </View>
 );
 
-const InputField = ({fieldKey, ...rest}) => {
+const InputField = ({refToPass, isSubmitting, fieldError, ...rest}) => {
   return (
-    <InputFieldWrapper fieldKey={fieldKey}>
+    <InputFieldWrapper fieldError={fieldError}>
       <TextInput
+        ref={refToPass}
         style={styles.textInput}
         clearButtonMode="always"
         autoCorrect={false}
         blurOnSubmit={false}
         returnKeyType="done"
+        editable={isSubmitting}
         {...rest}
       />
     </InputFieldWrapper>
--- Lejr/screens/dashboard/Home.js
+++ Lejr/screens/dashboard/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Button, StyleSheet} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 export default function Home({navigation}) {
   console.log('Arrived at Home!');
 
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Button
+        title="Sign out"
+        onPress={() => {
+          auth()
+            .signOut()
+            .then(() => console.log('User signed out!'));
+        }}
+      />
     </View>
   );
 }
