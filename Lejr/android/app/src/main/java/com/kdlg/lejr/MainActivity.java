4f74cce381ffe7a08baa21921de4a6859dc89029
Kevin Liu <kdliu00@gmail.com>
2021-02-04 23:08:56 -0800

    Native Android splash, needs verification

--- Lejr/android/app/src/main/java/com/kdlg/lejr/MainActivity.java
+++ Lejr/android/app/src/main/java/com/kdlg/lejr/MainActivity.java
@@ -1,8 +1,17 @@
 package com.kdlg.lejr;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    // Make sure this is before calling super.onCreate
+    setTheme(R.style.AppTheme);
+    super.onCreate(savedInstanceState);
+  }
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
