fabdc1b6a21c33cd89958ba1673fd31db7b207b6
Kevin Liu <kdliu00@gmail.com>
2021-02-04 21:39:39 -0800

    Removed react-native-splash-screen

--- Lejr/android/app/src/main/java/com/kdlg/lejr/MainApplication.java
+++ Lejr/android/app/src/main/java/com/kdlg/lejr/MainApplication.java
@@ -14,7 +14,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import org.reactnative.camera.RNCameraPackage;
 
 // import com.reactnativecommunity.cameraroll.CameraRollPackage;
